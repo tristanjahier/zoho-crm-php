1190703c2d6b8b7d22d7942e8acf9165034b19d3
Tristan Jahier <tristan.jahier@gmail.com>
2016-04-05 19:01:05 +0200

    Unwrap response automatically for getRecordById
    Remove the useless container level because its response returns a single element.

--- src/Methods/GetRecordById.php
+++ src/Methods/GetRecordById.php
@@ -4,5 +4,10 @@ namespace Zoho\CRM\Methods;
 
 class GetRecordById extends GetRecords
 {
-    //
+    public static function tidyResponse(array $response, $module)
+    {
+        $result = parent::tidyResponse($response, $module);
+        // Unwrap in case of single element
+        return count($result) === 1 ? $result[0] : $result;
+    }
 }
