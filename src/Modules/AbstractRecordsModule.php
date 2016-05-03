e6dfee8cd95241f509cfc42b1ffcec3b3c9a7606
Tristan Jahier <tristan.jahier@gmail.com>
2016-05-03 19:11:04 +0200

    Group common methods in AbstractRecordsModule

---
+++ src/Modules/AbstractRecordsModule.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Zoho\CRM\Modules;
+
+use Zoho\CRM\Core\IdList;
+use Zoho\CRM\Core\XmlBuilder;
+
+abstract class AbstractRecordsModule extends AbstractModule
+{
+    public function getById($id)
+    {
+        return $this->request('getRecordById', ['id' => $id]);
+    }
+
+    public function getByIds(array $id_list)
+    {
+        return $this->request('getRecordById', ['idlist' => new IdList($id_list)]);
+    }
+
+    public function getMine()
+    {
+        return $this->request('getMyRecords', [], true);
+    }
+
+    public function search($criteria)
+    {
+        return $this->request('searchRecords', ['criteria' => "($criteria)"]);
+    }
+
+    public function getBy($key, $value)
+    {
+        return $this->search(urlencode($key) . ':' . urlencode($value));
+    }
+
+    public function exists($id)
+    {
+        return $this->getById($id) !== null;
+    }
+
+    public function insert($data)
+    {
+        return $this->insertMany([$data]);
+    }
+
+    public function insertMany($data)
+    {
+        return $this->request('insertRecords', [
+            'version' => 4, // Required for full multiple records support
+            'duplicateCheck' => 1,
+            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), $data)
+        ]);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->request('updateRecords', [
+            'version' => 2, // Required for single record support
+            'id' => $id,
+            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), [$data])
+        ]);
+    }
+
+    public function updateMany($data)
+    {
+        return $this->request('updateRecords', [
+            'version' => 4, // Required for full multiple records support
+            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), $data)
+        ]);
+    }
+}
--- src/Modules/Leads.php
+++ src/Modules/Leads.php
@@ -2,10 +2,7 @@
 
 namespace Zoho\CRM\Modules;
 
-use Zoho\CRM\Core\IdList;
-use Zoho\CRM\Core\XmlBuilder;
-
-class Leads extends AbstractModule
+class Leads extends AbstractRecordsModule
 {
     protected static $supported_methods = [
         'getFields',
@@ -16,55 +13,4 @@ class Leads extends AbstractModule
         'insertRecords',
         'updateRecords'
     ];
-
-    public function getById($id)
-    {
-        return $this->request('getRecordById', ['id' => $id]);
-    }
-
-    public function getByIds(array $id_list)
-    {
-        return $this->request('getRecordById', ['idlist' => new IdList($id_list)]);
-    }
-
-    public function getMine()
-    {
-        return $this->request('getMyRecords', [], true);
-    }
-
-    public function search($criteria)
-    {
-        return $this->request('searchRecords', ['criteria' => "($criteria)"]);
-    }
-
-    public function getBy($key, $value)
-    {
-        return $this->search(urlencode($key) . ':' . urlencode($value));
-    }
-
-    public function insert($data)
-    {
-        return $this->request('insertRecords', [
-            'version' => 4, // Required for full multiple records support
-            'duplicateCheck' => 1,
-            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), $data)
-        ]);
-    }
-
-    public function update($id, $data)
-    {
-        return $this->request('updateRecords', [
-            'version' => 2, // Required for single record support
-            'id' => $id,
-            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), [$data])
-        ]);
-    }
-
-    public function updateMany($data)
-    {
-        return $this->request('updateRecords', [
-            'version' => 4, // Required for full multiple records support
-            'xmlData' => XmlBuilder::buildRecords(self::moduleName(), $data)
-        ]);
-    }
 }
