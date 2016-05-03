494af313e15bb354676cbf96c45869f7874bb0d4
Tristan Jahier <tristan.jahier@gmail.com>
2016-05-03 15:17:17 +0200

    Fix for GetModules and GetFields responses

--- src/Methods/GetModules.php
+++ src/Methods/GetModules.php
@@ -2,10 +2,13 @@
 
 namespace Zoho\CRM\Methods;
 
+use Zoho\CRM\Core\ApiResponseType;
 use Zoho\CRM\Core\Request;
 
 class GetModules extends AbstractMethod
 {
+    protected static $response_type = ApiResponseType::OTHER;
+
     public static function tidyResponse(array $response, Request $request)
     {
         $entries = [];
