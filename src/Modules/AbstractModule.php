6d8764840acb4148aee0a4839e41570055e29602
Tristan Jahier <tristan.jahier@gmail.com>
2016-05-03 17:31:08 +0200

    Defer "getFields" methods in a separate (proxy) module

--- src/Modules/AbstractModule.php
+++ src/Modules/AbstractModule.php
@@ -5,6 +5,7 @@ namespace Zoho\CRM\Modules;
 use Zoho\CRM\Client as ZohoClient;
 use Zoho\CRM\Core\BaseClassStaticHelper;
 use Zoho\CRM\Core\UrlParameters;
+use Zoho\CRM\Modules\ModuleFields;
 use Doctrine\Common\Inflector\Inflector;
 
 abstract class AbstractModule extends BaseClassStaticHelper
@@ -17,12 +18,19 @@ abstract class AbstractModule extends BaseClassStaticHelper
 
     private $owner;
 
+    private $fields;
+
     protected $parameters_accumulator;
 
     public function __construct(ZohoClient $owner)
     {
         $this->owner = $owner;
         $this->parameters_accumulator = new UrlParameters();
+
+        // Add a meta module to retrieve this module's fields
+        if (!($this instanceof ModuleFields)) {
+            $this->fields = new ModuleFields($owner, self::moduleName());
+        }
     }
 
     public static function moduleName()
@@ -54,50 +62,21 @@ abstract class AbstractModule extends BaseClassStaticHelper
         return $this->owner;
     }
 
+    public function fields()
+    {
+        return $this->fields;
+    }
+
+    private function managedModule()
+    {
+        return $this instanceof AbstractProxyModule ? $this->mandatedModule() : self::moduleName();
+    }
+
     protected function request($method, array $params = [], $pagination = false)
     {
         $params = $this->parameters_accumulator->extend($params)->toArray();
         $this->parameters_accumulator->reset();
-        return $this->owner->request(self::moduleName(), $method, $params, $pagination);
-    }
-
-    public function getFields(array $params = [], callable $filter = null)
-    {
-        $sections = $this->request('getFields', $params);
-
-        if (isset($filter)) {
-            foreach($sections as &$section) {
-                $section['FL'] = array_filter($section['FL'], $filter);
-                if (empty($section['FL']))
-                    unset($section['FL']);
-            }
-        }
-
-        return $sections;
-    }
-
-    public function getNativeFields()
-    {
-        return $this->getFields([], function($field) {
-            return $field['customfield'] === 'false';
-        });
-    }
-
-    public function getCustomFields()
-    {
-        return $this->getFields([], function($field) {
-            return $field['customfield'] === 'true';
-        });
-    }
-
-    public function getSummaryFields()
-    {
-        return $this->getFields(['type' => 1]);
-    }
-
-    public function getMandatoryFields()
-    {
-        return $this->getFields(['type' => 2]);
+        return $this->owner->request($this->managedModule(), $method, $params, $pagination);
     }
 
     public function orderBy($column, $order = 'asc')
@@ -118,7 +97,7 @@ abstract class AbstractModule extends BaseClassStaticHelper
 
     public function selectColumns(array $columns)
     {
-        $selection_str = static::moduleName() . '(' . implode(',', $columns) . ')';
+        $selection_str = $this->managedModule() . '(' . implode(',', $columns) . ')';
         $this->parameters_accumulator['selectColumns'] = $selection_str;
         return $this;
     }
---
+++ src/Modules/AbstractProxyModule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zoho\CRM\Modules;
+
+abstract class AbstractProxyModule extends AbstractModule
+{
+    protected $mandated_module;
+
+    public function __construct($owner, $module)
+    {
+        $this->mandated_module = $module;
+        parent::__construct($owner);
+    }
+
+    public function mandatedModule()
+    {
+        return $this->mandated_module;
+    }
+}
