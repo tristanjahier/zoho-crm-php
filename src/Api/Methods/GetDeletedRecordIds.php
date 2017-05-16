f41692ef6d034b8f44207bac93696cb5fe21fc00
Tristan Jahier <tristan.jahier@gmail.com>
2017-05-16 21:04:47 +0200

    Fix GetDeletedRecordIds::responseContainsData()

--- src/Api/Methods/GetDeletedRecordIds.php
+++ src/Api/Methods/GetDeletedRecordIds.php
@@ -11,9 +11,15 @@ class GetDeletedRecordIds extends AbstractMethod
 
     public static function responseContainsData(array $response)
     {
+        if (isset($response['response']['nodata'])) {
+            return false;
+        }
+
+        // When going beyond the last page of results,
+        // the API does not respond with a "nodata" message, as you would expect.
+        // It just returns 'true' as the content of "DeletedIDs"... i.e.: {"DeletedIDs": true}
         $result = $response['response']['result']['DeletedIDs'];
 
-        // When there is no (more) deleted ID, it just returns 'true'. i.e.: {"DeletedIDs":true}
         return isset($result) && ! empty($result) && $result !== true;
     }
 
