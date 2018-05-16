45fb5c07acf49195be4dd14bd7327843c8f04aae
Haru0 <haruni000@gmail.com>
2018-05-16 08:48:04 +0200

    Remove redundant fetch()->getContent()

--- src/Api/Modules/PotStageHistory.php
+++ src/Api/Modules/PotStageHistory.php
@@ -14,6 +14,6 @@ class PotStageHistory extends AbstractRecordsModule
 
     public function getPotentialStageHistory($potential_id)
     {
-        return $this->getRelatedById('Potential', $potential_id)->fetch()->getContent();
+        return $this->getRelatedById('Potential', $potential_id);
     }
 }
