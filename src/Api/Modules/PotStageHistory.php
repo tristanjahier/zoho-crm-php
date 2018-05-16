de0c29b8f2caf2f54f1b14191a352429c7ec38e5
Haru0 <haruni000@gmail.com>
2018-05-16 08:49:00 +0200

    Typo fix: Plurify module name

--- src/Api/Modules/PotStageHistory.php
+++ src/Api/Modules/PotStageHistory.php
@@ -14,6 +14,6 @@ class PotStageHistory extends AbstractRecordsModule
 
     public function getPotentialStageHistory($potential_id)
     {
-        return $this->getRelatedById('Potential', $potential_id);
+        return $this->getRelatedById('Potentials', $potential_id);
     }
 }
