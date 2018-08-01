9b1217378aa8d237259e28187751d03bd87868ea
Tristan Jahier <tristan.jahier@gmail.com>
2018-08-01 16:58:38 +0200

    Make AbstractMethod use ClassShortNameTrait

--- src/Api/Methods/AbstractMethod.php
+++ src/Api/Methods/AbstractMethod.php
@@ -5,18 +5,21 @@ namespace Zoho\Crm\Api\Methods;
 use Zoho\Crm\Api\ResponseDataType;
 use Zoho\Crm\Api\HttpVerb;
 use Zoho\Crm\Api\Query;
+use Zoho\Crm\Support\ClassShortNameTrait;
 
 abstract class AbstractMethod implements MethodInterface
 {
+    use ClassShortNameTrait;
+
     protected static $response_type = ResponseDataType::RECORDS;
 
     protected static $multiple_records = true;
 
     protected static $http_verb = HttpVerb::GET;
 
-    final public static function getMethodName()
+    public static function name()
     {
-        return (new \ReflectionClass(static::class))->getShortName();
+        return lcfirst(self::getClassShortName());
     }
 
     public static function getResponseDataType()
