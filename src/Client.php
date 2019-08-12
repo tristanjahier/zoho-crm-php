8ecc8f10818c5ab3be0dce7777cfcf519a4aa4cf
Tristan Jahier <tristan.jahier@gmail.com>
2019-08-12 16:02:41 +0200

    Create QueryProcessor to handle queries execution

--- src/Client.php
+++ src/Client.php
@@ -2,12 +2,9 @@
 
 namespace Zoho\Crm;
 
-use Zoho\Crm\Support\Helper;
 use Zoho\Crm\Api\Modules\AbstractModule;
 use Zoho\Crm\Api\Query;
 use Doctrine\Common\Inflector\Inflector;
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Exception\RequestException;
 
 /**
  * Zoho CRM API client. Main class of the library.
@@ -64,11 +61,8 @@ class Client
     /** @var string The API authentication token */
     protected $authToken;
 
-    /** @var \GuzzleHttp\Client The Guzzle client instance to make HTTP requests */
-    protected $httpClient;
-
-    /** @var int The number of API requests made by the client */
-    protected $requestCount = 0;
+    /** @var QueryProcessor The query processor */
+    protected $queryProcessor;
 
     /** @var Preferences The client preferences container */
     protected $preferences;
@@ -107,23 +101,11 @@ class Client
             $this->setEndpoint($endpoint);
         }
 
-        $this->setupHttpClient();
+        $this->queryProcessor = new QueryProcessor($this);
 
         $this->preferences = new Preferences();
 
         $this->attachDefaultModules();
-    }
-
-    /**
-     * Create and configure the Guzzle client.
-     *
-     * @return void
-     */
-    private function setupHttpClient()
-    {
-        $this->httpClient = new GuzzleClient([
-            'base_uri' => $this->endpoint
-        ]);
     }
 
     /**
@@ -250,23 +232,13 @@ class Client
     }
 
     /**
-     * Reset the API request counter.
-     *
-     * @return void
-     */
-    public function resetRequestCount()
-    {
-        $this->requestCount = 0;
-    }
-
-    /**
      * Get the number of API requests made by the client.
      *
      * @return int
      */
     public function getRequestCount()
     {
-        return $this->requestCount;
+        return $this->queryProcessor->getRequestCount();
     }
 
     /**
@@ -302,7 +274,7 @@ class Client
         $this->endpoint = $endpoint . '/';
 
         // Re-create the HTTP client because the base URI has changed
-        $this->setupHttpClient();
+        $this->queryProcessor->setupHttpClient();
     }
 
     /**
@@ -340,6 +312,16 @@ class Client
         } else {
             $this->authToken = $authToken;
         }
+    }
+
+    /**
+     * Get the query processor.
+     *
+     * @return QueryProcessor
+     */
+    public function getQueryProcessor()
+    {
+        return $this->queryProcessor;
     }
 
     /**
@@ -423,52 +405,7 @@ class Client
      */
     public function executeQuery(Query $query)
     {
-        if ($query->isPaginated()) {
-            $paginator = $query->getPaginator();
-            $paginator->fetchAll();
-            return $paginator->getAggregatedResponse();
-        }
-
-        $query->validate();
-
-        // Check if the requested module and method are both supported
-        if (! $this->supports($query->getModule())) {
-            throw new Exceptions\UnsupportedModuleException($query->getModule());
-        }
-
-        if (! class_exists($methodClass = Helper::getMethodClass($query->getMethod()))) {
-            throw new Exceptions\UnsupportedMethodException($query->getMethod());
-        }
-
-        // Determine the HTTP verb to use based on the API method
-        $httpVerb = $methodClass::getHttpVerb();
-
-        // Add auth token at the last moment to avoid exposing it in the error log messages
-        $query->param('authtoken', $this->authToken);
-
-        // Perform the HTTP request
-        try {
-            $response = $this->httpClient->request($httpVerb, $query->buildUri());
-            $this->requestCount++;
-        } catch (RequestException $e) {
-            if ($this->preferences->isEnabled('exception_messages_obfuscation')) {
-                // Sometimes the auth token is included in the exception message by Guzzle.
-                // This exception message could end up in many "unsafe" places like server logs,
-                // error monitoring services, company internal communication etc.
-                // For this reason we must remove the auth token from the exception message.
-
-                throw $this->obfuscateExceptionMessage($e);
-            }
-
-            throw $e;
-        }
-
-        // Clean the response
-        $rawContent = $response->getBody()->getContents();
-        $content = Api\ResponseParser::clean($query, $rawContent);
-        $response = new Api\Response($query, $content, $rawContent);
-
-        return $response;
+        return $this->queryProcessor->executeQuery($query);
     }
 
     /**
@@ -500,34 +437,6 @@ class Client
         }
 
         return $response->getContent();
-    }
-
-    /**
-     * Obfuscate an exception by removing the API auth token from its message.
-     *
-     * It will actually create a copy of the original exception because
-     * exception messages are immutable.
-     *
-     * @param \GuzzleHttp\Exception\RequestException $e The exception to obfuscate
-     * @return \GuzzleHttp\Exception\RequestException
-     */
-    private function obfuscateExceptionMessage(RequestException $e)
-    {
-        // If the exception message does not contain sensible data, just let it through.
-        if (mb_strpos($e->getMessage(), 'authtoken='.$this->authToken) === false) {
-            return $e;
-        }
-
-        $safeMessage = str_replace('authtoken='.$this->authToken, 'authtoken=***', $e->getMessage());
-        $class = get_class($e);
-
-        return new $class(
-            $safeMessage,
-            $e->getRequest(),
-            $e->getResponse(),
-            $e->getPrevious(),
-            $e->getHandlerContext()
-        );
     }
 
     /**
---
+++ src/QueryProcessor.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Zoho\Crm;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request as HttpRequest;
+use GuzzleHttp\Psr7\Response as HttpResponse;
+use Zoho\Crm\Api\Query;
+use Zoho\Crm\Api\ResponseParser;
+use Zoho\Crm\Api\Response;
+use Zoho\Crm\Exceptions\UnsupportedModuleException;
+use Zoho\Crm\Exceptions\UnsupportedMethodException;
+use Zoho\Crm\Support\Helper;
+
+/**
+ * The query processor.
+ */
+class QueryProcessor
+{
+    /** @var Client The client to which this processor is attached */
+    protected $client;
+
+    /** @var \GuzzleHttp\Client The Guzzle client instance to make HTTP requests */
+    protected $httpClient;
+
+    /** @var int The number of API requests made */
+    protected $requestCount = 0;
+
+    /**
+     * The constructor.
+     *
+     * @param Client $client The client to which it is attached
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+
+        $this->setupHttpClient();
+    }
+
+    /**
+     * Create and configure the Guzzle client.
+     *
+     * @return void
+     */
+    public function setupHttpClient()
+    {
+        $this->httpClient = new GuzzleClient([
+            'base_uri' => $this->client->getEndpoint()
+        ]);
+    }
+
+    /**
+     * Reset the API request counter.
+     *
+     * @return void
+     */
+    public function resetRequestCount()
+    {
+        $this->requestCount = 0;
+    }
+
+    /**
+     * Get the number of API requests made.
+     *
+     * @return int
+     */
+    public function getRequestCount()
+    {
+        return $this->requestCount;
+    }
+
+    /**
+     * Execute a given query and get a formal and generic response object.
+     *
+     * @param Api\Query $query The query to execute
+     * @return Api\Response
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
+     */
+    public function executeQuery(Query $query)
+    {
+        if ($query->isPaginated()) {
+            return $this->executePaginatedQuery($query);
+        }
+
+        $this->validateQuery($query);
+
+        $request = $this->createHttpRequest($query);
+
+        // Perform the HTTP request
+        try {
+            $response = $this->httpClient->send($request);
+            $this->requestCount++;
+        } catch (RequestException $e) {
+            if ($this->client->preferences()->isEnabled('exception_messages_obfuscation')) {
+                // Sometimes the auth token is included in the exception message by Guzzle.
+                // This exception message could end up in many "unsafe" places like server logs,
+                // error monitoring services, company internal communication etc.
+                // For this reason we must remove the auth token from the exception message.
+
+                throw $this->obfuscateExceptionMessage($e);
+            }
+
+            throw $e;
+        }
+
+        return $this->parseResponse($response, $query);
+    }
+
+    /**
+     * Validate that a query is valid before sending the request to the API.
+     *
+     * @param Api\Query $query The query to validate
+     * @return void
+     *
+     * @throws \Zoho\Crm\Exceptions\UnsupportedModuleException
+     * @throws \Zoho\Crm\Exceptions\UnsupportedMethodException
+     */
+    private function validateQuery(Query $query)
+    {
+        // Internal validation logic
+        $query->validate();
+
+        // Check if the requested module and method are both supported
+        if (! $this->client->supports($query->getModule())) {
+            throw new UnsupportedModuleException($query->getModule());
+        }
+
+        if (! class_exists(Helper::getMethodClass($query->getMethod()))) {
+            throw new UnsupportedMethodException($query->getMethod());
+        }
+    }
+
+    /**
+     * Transform a query into an HTTP request.
+     *
+     * @param Api\Query $query The query
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    private function createHttpRequest(Query $query)
+    {
+        // Determine the HTTP verb to use from the API method handler
+        $methodClass = Helper::getMethodClass($query->getMethod());
+        $httpVerb = $methodClass::getHttpVerb();
+
+        // Add auth token at the last moment to avoid exposing it in the error log messages
+        $query->param('authtoken', $this->client->getAuthToken());
+
+        return new HttpRequest($httpVerb, $query->buildUri());
+    }
+
+    /**
+     * Parse an API response and transform its content into a relevant data object.
+     *
+     * @param \GuzzleHttp\Psr7\Response $httpResponse The API response to read
+     * @param Api\Query $query The origin query
+     * @return Api\Response
+     */
+    private function parseResponse(HttpResponse $httpResponse, Query $query)
+    {
+        $rawContent = $httpResponse->getBody()->getContents();
+        $content = ResponseParser::clean($query, $rawContent);
+
+        return new Response($query, $content, $rawContent);
+    }
+
+    /**
+     * Execute a paginated query.
+     *
+     * @param Api\Query $query The query to execute
+     * @return Api\Response
+     */
+    private function executePaginatedQuery(Query $query)
+    {
+        $paginator = $query->getPaginator();
+        $paginator->fetchAll();
+
+        return $paginator->getAggregatedResponse();
+    }
+
+    /**
+     * Obfuscate an exception by removing the API auth token from its message.
+     *
+     * It will actually create a copy of the original exception because
+     * exception messages are immutable.
+     *
+     * @param \GuzzleHttp\Exception\RequestException $e The exception to obfuscate
+     * @return \GuzzleHttp\Exception\RequestException
+     */
+    private function obfuscateExceptionMessage(RequestException $e)
+    {
+        // If the exception message does not contain sensible data, just let it through.
+        if (mb_strpos($e->getMessage(), 'authtoken='.$this->authToken) === false) {
+            return $e;
+        }
+
+        $safeMessage = str_replace('authtoken='.$this->authToken, 'authtoken=***', $e->getMessage());
+        $class = get_class($e);
+
+        return new $class(
+            $safeMessage,
+            $e->getRequest(),
+            $e->getResponse(),
+            $e->getPrevious(),
+            $e->getHandlerContext()
+        );
+    }
+}
