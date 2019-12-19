ebf1e780412beb16bade7fecd60f22331681815d
Tristan Jahier <tristan.jahier@gmail.com>
2019-12-19 03:38:35 +0100

    Add Records\ListDeletedQuery::after()

--- src/V2/Records/ListDeletedQuery.php
+++ src/V2/Records/ListDeletedQuery.php
@@ -4,6 +4,7 @@ namespace Zoho\Crm\V2\Records;
 
 use Zoho\Crm\Contracts\ResponseTransformerInterface;
 use Zoho\Crm\Contracts\PaginatedQueryInterface;
+use Zoho\Crm\Support\Helper;
 use Zoho\Crm\V2\Traits\HasPagination;
 
 /**
@@ -31,5 +32,45 @@ class ListDeletedQuery extends AbstractQuery implements PaginatedQueryInterface
     public function getResponseTransformer(): ?ResponseTransformerInterface
     {
         return new DeletedRecordListTransformer();
+    }
+
+    /**
+     * Set the minimum deletion date.
+     *
+     * @param \DateTimeInterface|string|null $date A date object or a valid string
+     * @return $this
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function after($date)
+    {
+        if (is_null($date)) {
+            return $this->removeHeader('If-Modified-Since');
+        }
+
+        $date = $this->getValidatedDateObject($date);
+
+        return $this->setHeader('If-Modified-Since', $date->format(DATE_ATOM));
+    }
+
+    /**
+     * Ensure to get a valid DateTime object.
+     *
+     * @param \DateTimeInterface|string $date A date object or a valid string
+     * @return \DateTime
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getValidatedDateObject($date)
+    {
+        if (! Helper::isValidDateInput($date)) {
+            throw new \InvalidArgumentException('Date must implement DateTimeInterface or be a valid date string.');
+        }
+
+        if (is_string($date)) {
+            return new \DateTime($date);
+        }
+
+        return $date;
     }
 }
