1ecaf3ab1fd6166fc68e2dd617bb946d15c01431
Salvador Briones <salvador.briones@selectra.info|mexidense@gmail.com>
2020-01-23 23:38:55 +0100

    feat: create related records subAPI

---
+++ src/V2/RelatedRecords/SubApi.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Zoho\Crm\V2\RelatedRecords;
+
+use Doctrine\Common\Inflector\Inflector;
+use Zoho\Crm\V2\AbstractSubApi;
+
+/**
+ * Helper for the Record APIs.
+ */
+class SubApi extends AbstractSubApi
+{
+    /**
+     * Create a module helper.
+     *
+     * @param string $name The name of the module
+     * @return Module
+     */
+    public function module(string $name): Module
+    {
+        return new Module($this->client, $name);
+    }
+
+    /**
+     * Get a module helper as a public property.
+     *
+     * The module name needs to be written in camel case.
+     * Example: `$client->related_records->priceBooks` instead of `$client->related_records->module('PriceBooks')`.
+     *
+     * @param string $name The name of the module in camel case
+     * @return Module
+     */
+    public function __get(string $name)
+    {
+        return $this->module(Inflector::classify($name));
+    }
+}
