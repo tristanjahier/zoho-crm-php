a89e929a8373090d8732463f4cc537fd6d1abdfc
Tristan Jahier <tristan.jahier@gmail.com>
2020-02-11 17:15:57 +0100

    Add doc comments in V1\Preferences

--- src/V1/Preferences.php
+++ src/V1/Preferences.php
@@ -11,8 +11,26 @@ class Preferences extends PreferencesContainer
 {
     /** @var array The available preferences and their default values */
     protected static $defaults = [
+        /**
+         * Hide the authentication token in exception messages.
+         *
+         * @var bool
+         */
         'exception_messages_obfuscation' => false,
+
+        /**
+         * Use concurrency by default in all auto-paginated queries created from the client.
+         *
+         * @var bool
+         */
         'concurrent_pagination_by_default' => false,
+
+        /**
+         * The default number of concurrent requests to use.
+         * Must be a positive non-zero integer.
+         *
+         * @var int
+         */
         'default_concurrency' => 5,
     ];
 }
