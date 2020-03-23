b73f3f1ced02e146df16c1cfc55d79c9e71bac2f
Tristan Jahier <tristan.jahier@gmail.com>
2020-03-23 17:48:23 +0100

    Add UnwrapDataTransformer

---
+++ src/V2/UnwrapDataTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zoho\Crm\V2;
+
+use Zoho\Crm\Contracts\ResponseTransformerInterface;
+use Zoho\Crm\Contracts\QueryInterface;
+
+/**
+ * A very basic transformer to extract the value of the "data" key.
+ */
+class UnwrapDataTransformer implements ResponseTransformerInterface
+{
+    /**
+     * @inheritdoc
+     *
+     * @return mixed|null
+     */
+    public function transformResponse($content, QueryInterface $query)
+    {
+        return $content['data'] ?? null;
+    }
+}
