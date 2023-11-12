e96b0e0101cc90c1b96247ea030d1ee319436bd4
Tristan Jahier <tristan.jahier@gmail.com>
2023-11-12 16:01:13 +0100

    Add a "fixed" access token broker
    This kinda defeats the original purpose of the token broker component.
    However it can be useful for testing!

---
+++ src/FixedAccessTokenBroker.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zoho\Crm;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use Zoho\Crm\Contracts\AccessTokenBrokerInterface;
+
+class FixedAccessTokenBroker implements AccessTokenBrokerInterface
+{
+    /** @var The API access token */
+    protected string $accessToken;
+
+    /** @var The access token expiry date */
+    protected DateTimeImmutable $expiryDate;
+
+    /**
+     * The constructor.
+     *
+     * @param string $accessToken The fixed access token to serve
+     * @param \DateTimeInterface $expiryDate The fixed expiry date to serve
+     */
+    public function __construct(string $accessToken, DateTimeInterface $expiryDate)
+    {
+        $this->accessToken = $accessToken;
+
+        // Create an immutable copy from any type implementing DateTimeInterface.
+        $this->expiryDate = DateTimeImmutable::createFromFormat(
+            DateTimeInterface::ATOM,
+            $expiryDate->format(DateTimeInterface::ATOM)
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAccessTokenWithExpiryDate(): array
+    {
+        return [$this->accessToken, $this->expiryDate];
+    }
+}
